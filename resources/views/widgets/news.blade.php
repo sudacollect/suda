43900e64e3baf7f2235fe2f041523e2a64cb103c
devops <uulele@gmail.com>
2023-12-27 17:29:11 +0800

    update news date

--- resources/views/widgets/news.blade.php
+++ resources/views/widgets/news.blade.php
@@ -14,7 +14,7 @@
             
               <li class="list-group-item d-flex justify-content-between align-items-center">
                 <a href="{{ $item->get_permalink() }}" target=_blank>{{ $item->get_title() }}</a>
-                <small>{{ $item->get_date('Y-m-d') }}</small>
+                <small>{{ $item->get_date('d/m') }}</small>
               </li>
             @endforeach
               </ul>
